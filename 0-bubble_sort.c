5204810b832bfea66e16d28866df65edf47a3dc9
GuarickGit <10615@holbertonstudents.com>
2025-04-11 14:54:42 +0200

    Update 0-bubble_sort.c | Add comments

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,15 +19,24 @@ void bubble_sort(int *array, size_t size)
 	size_t j;
 	int temp;
 
+	/* Boucle qui effectue plusieurs passages dans le tableau */
 	for (i = 0; i < size - 1; i++)
 	{
+		/*
+		 * À chaque passage, on compare les éléments adjacents.
+		 * On s'arrête à size - i - 1 car les plus grands éléments
+		 * sont déjà triés et placés à la fin du tableau.
+		 */
 		for (j = 0; j < size - i - 1; j++)
 		{
+			/* Si l'élément courant est plus grand que le suivant */
 			if (array[j] > array[j + 1])
 			{
+				/* On échange les deux éléments */
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				/* On affiche le tableau après chaque échange */
 				print_array(array, size);
 			}
 		}
